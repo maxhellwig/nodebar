5d5268f6e4521638e87d5a2638af4051a64f1e04
Max Hellwig <mxhellwig@gmail.com>
2019-02-28 21:51:56 +0100

    Format clock output

--- src/Plugins/Clock.ts
+++ src/Plugins/Clock.ts
@@ -1,7 +1,8 @@
 import Base from "./Base";
 
 export default class Clock extends Base {
-    cycle() {
-        this.fullText = Date();
-    }
+  cycle() {
+    const now: Date = new Date();
+    this.full_text = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+  }
 }
