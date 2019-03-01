47fd6321bf005028d4a0e15058fbdf0de94da0ae
Max Hellwig <mxhellwig@gmail.com>
2019-03-01 09:50:26 +0100

    Introduce momentjs to easily format date-time, fuck you JavaScript..

--- src/Plugins/Clock.ts
+++ src/Plugins/Clock.ts
@@ -1,8 +1,9 @@
 import Base from "./Base";
+import moment = require("moment");
 
 export default class Clock extends Base {
   cycle() {
-    const now: Date = new Date();
-    this.full_text = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    process.env.TZ = "Europe/Berlin";
+    this.full_text = `${moment().format('YYYY-MM-DD hh:mm:ss')}`;
   }
 }
