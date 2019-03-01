9328bbd3c7f86a8af97a2674c96a9f43561f342e
Max Hellwig <mail@maxhellwig.de>
2019-03-01 12:34:51 +0100

    Remove unused timezone setting

--- src/Plugins/Clock.ts
+++ src/Plugins/Clock.ts
@@ -3,7 +3,6 @@ import moment = require("moment");
 
 export default class Clock extends Base {
   cycle() {
-    process.env.TZ = "Europe/Berlin";
     this.full_text = `${moment().format('YYYY-MM-DD hh:mm:ss')}`;
   }
 }
