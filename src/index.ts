eaccaf19182d587a1155e874cb792b6cf9f836a9
Max Hellwig <mail@maxhellwig.de>
2019-03-02 23:40:00 +0100

    Rename uname to hostname plugin in index list

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ import Net from "./Plugins/Net";
 function main() {
   const plugins: Array<Base> = [];
   const clock: Base = new Clock("Clock", 1);
-  const uname: Base = new Hostname("Hostname", 3600);
+  const hostname: Base = new Hostname("Hostname", 3600);
   const uptime: Base = new Uptime("Uptime", 1);
   const enp0s3: Base = new Net("enp0s3", 3);
   const lo: Base = new Net("lo", 3);
-  plugins.push(enp0s3, lo, uname, uptime, clock);
+  plugins.push(enp0s3, lo, hostname, uptime, clock);
   const app = new App(plugins);
   app.run();
 }
