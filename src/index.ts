33d1a20bc206106a6bf1e9b7df4d44eaec57bc96
Max Hellwig <mail@maxhellwig.de>
2019-03-03 16:27:31 +0100

    Add release plugin to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Base from "./Plugins/Base";
 import Clock from "./Plugins/Clock";
 import Hostname from "./Plugins/Hostname";
+import Release from "./Plugins/OSRelease";
 import App from "./App";
 import Uptime from "./Plugins/Uptime";
 import Net from "./Plugins/Net";
@@ -10,10 +11,10 @@ function main() {
   const plugins: Array<Base> = [];
   const clock: Base = new Clock("Clock", 1);
   const hostname: Base = new Hostname("Hostname", 3600);
+  const release: Base = new Release("Release", 3600);
   const uptime: Base = new Uptime("Uptime", 1);
   const enp0s3: Base = new Net("enp0s3", 3);
-  const lo: Base = new Net("lo", 3);
-  plugins.push(enp0s3, lo, hostname, uptime, clock);
+  plugins.push(enp0s3, release, hostname, uptime, clock);
   const app = new App(plugins);
   app.run();
 }
