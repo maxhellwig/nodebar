5fcd722b8cf8f2366e76cd0f95896ad952052e2f
Max Hellwig <mxhellwig@gmail.com>
2019-03-06 21:44:30 +0100

    Introduce logging and a rerun() capability

--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,26 @@
 import Base from "./Plugins/Base";
 import NoPlugin from "./NoPlugin";
+import * as winston from "winston";
+import Timeout = NodeJS.Timeout;
 
-const Sentry = require("@sentry/node");
 const HEADER: string = JSON.stringify({ "version": 1, "stop_signal": 10, "cont_signal": 12, "click_events": true });
 
-const DEBUG: boolean | undefined = process.env.DEBUG === "true";
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 export default class App {
 
 
   private readonly plugins: Array<Base>;
+  private intervalHandler!: Timeout;
+  private static appLogger: winston.Logger;
 
   /**
    *
@@ -17,10 +28,32 @@ export default class App {
    */
   constructor(plugins: Array<Base>) {
 
-    Sentry.init({ dsn: "https://d3e1feea8cbc4694a3a39b62e16f497a@sentry.io/1406827" });
     this.plugins = plugins;
+    App.appLogger = App.initiateLogger();
+    if (process.env.NODE_ENV !== "production") {
+      App.appLogger.add(new winston.transports.Console({
+        level: "debug",
+        format: winston.format.simple()
+      }));
+    }
   }
 
+  static initiateLogger(): winston.Logger {
+    return winston.createLogger({
+      level: "info",
+      format: winston.format.json(),
+      defaultMeta: { service: "App" },
+      transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log`
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" })
+      ]
+    });
+
+  }
 
   /**
    *
@@ -35,24 +68,38 @@ export default class App {
     return output;
   }
 
-
   run() {
     console.log(HEADER);
     console.log("[");
     console.log("[]");
-    if(this.plugins.length === 0 ) {
-      this.plugins.push(new NoPlugin("No plugins!", -1))
+    if (this.plugins.length === 0) {
+      this.plugins.push(new NoPlugin("No plugins!", 45000));
     }
-    this.plugins.forEach(function(plugin) {
-      plugin.run();
-    });
-    setInterval(() => {
-      let output: string;
-      output = this.collectOutput(this.plugins);
-      // remove the first comma in array
-      console.log(`,[${output}]`.replace("\[,", "\["));
-      output = "";
-    }, 1000);
+    try {
+      this.plugins.forEach(function(plugin) {
+        App.appLogger.debug(`${plugin.toString()}`);
+        plugin.run();
+      });
+      this.intervalHandler = setInterval(() => {
+        let output: string;
+        output = this.collectOutput(this.plugins);
+        // remove the first comma in array
+        console.log(`,[${output}]`.replace("\[,", "\["));
+        output = "";
+      }, 1000);
+    } catch (e) {
+      this.rerun(e);
+    }
+
+  }
+
+  rerun(e: Error) {
+    App.appLogger.warn(`rerun() was called! An uncatched exception from a plugins run has been thrown!`);
+    App.appLogger.error(e);
+    App.appLogger.warn(`Clearing infinite loop Interval`);
+    global.clearInterval(this.intervalHandler);
+    App.appLogger.warn(`Starting over!`);
+    this.run();
   }
 
 }
