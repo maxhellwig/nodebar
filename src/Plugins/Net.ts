fc1c9753c5bb6777b3def85dbcaeb032b5771dfc
Max Hellwig <mxhellwig@gmail.com>
2019-03-06 21:45:32 +0100

    Improve Net plugin in order to catch the interface not found exception

--- src/Plugins/Net.ts
+++ src/Plugins/Net.ts
@@ -1,22 +1,26 @@
 import Base from "./Base";
 import * as os from "os";
+import { NetworkInterfaceInfo } from "os";
+import { COLORS } from "../COLORS";
 
 export default class Net extends Base {
-
 
   constructor(name: string, ticks: number) {
     super(name, ticks);
   }
 
   cycle() {
-    const allInterfaces: any = os.networkInterfaces();
-    const concreteInterface = allInterfaces[this.name];
-
-    this.full_text = `${this.name} ${concreteInterface[0].address}`;
-    if (this.full_text !== "") {
-      this.color = "#00ff00";
-    } else {
-      this.color = "#ff0000";
+    const allInterfaces: { [index: string]: NetworkInterfaceInfo[] } = os.networkInterfaces();
+    let concreteInterface: NetworkInterfaceInfo[];
+    try {
+      concreteInterface = allInterfaces[this.name];
+      this.full_text = `${this.name} ${concreteInterface[0].address}`;
+      this.color = COLORS.GREEN;
+    } catch (e) {
+      this.full_text = `Network interface '${this.name}' not found!`;
+      this.short_text = `${this.name} not found!`;
+      this.color = COLORS.WHITE;
+      this.background = COLORS.RED;
     }
   }
 }
