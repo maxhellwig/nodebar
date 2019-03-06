86af3215e33aef021fdbf33b9a05eb7a448cce2f
Max Hellwig <mxhellwig@gmail.com>
2019-03-06 22:14:03 +0100

    Resort plugin instantiation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ import Net from "./Plugins/Net";
 
 function main() {
   const plugins: Array<Base> = [];
-  const clock: Base = new Clock("Clock", 1);
-  const hostname: Base = new Hostname("Hostname", 3600);
+  const enp0s3: Base = new Net("wlp4s0", 5);
   const release: Base = new Release("Release", 3600);
+  const hostname: Base = new Hostname("Hostname", 3600);
   const uptime: Base = new Uptime("Uptime", 1);
-  const enp0s3: Base = new Net("enp0s3", 3);
+  const clock: Base = new Clock("Clock", 1);
   plugins.push(enp0s3, release, hostname, uptime, clock);
   const app = new App(plugins);
   app.run();
