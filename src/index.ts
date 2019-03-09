320c722aef1321e3824f6f56a7dbccc54d654fab
Max Hellwig <mxhellwig@gmail.com>
2019-03-09 14:03:21 +0100

    Add Battery plugin to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import Base from "./Plugins/Base";
 import Clock from "./Plugins/Clock";
 import Hostname from "./Plugins/Hostname";
@@ -7,6 +8,7 @@ import Uptime from "./Plugins/Uptime";
 import Net from "./Plugins/Net";
 import * as winston from "winston";
 import { LOGPATH } from "./config";
+import Battery from "./Plugins/Battery";
 
 
 const logger = winston.createLogger({
@@ -85,10 +87,13 @@ function main() {
   const enp0s3: Base = new Net("enp0s3", 5);
   const release: Base = new Release("Release", 3600);
   const hostname: Base = new Hostname("Hostname", 3600);
+  const battery0: Base = new Battery("Bat0", 10, "Bat0");
+  const battery1: Base = new Battery("Bat1", 10, "Bat1");
   const uptime: Base = new Uptime("Uptime", 1);
   const clock: Base = new Clock("Clock", 1);
-  plugins.push(enp0s3, release, hostname, uptime, clock);
+  plugins.push(enp0s3, release, hostname, battery0, battery1, uptime, clock);
   const app = new App(plugins);
+  logger.info("Start nodebar");
   app.run();
 }
 
