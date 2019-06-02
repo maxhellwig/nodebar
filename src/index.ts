22450a7d41a7f51197c53d6cc8e1f1e39635b3ef
Max Hellwig <mxhellwig@gmail.com>
2019-06-02 13:36:09 +0200

    Update plugins

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,15 @@ export const app: App = new App([]);
 
 const main = (): void => {
   const plugins: BasePlugin[] = [];
-  const enp0s3: BasePlugin = new Net("enp0s3", 5);
+  const enp0s31f6: BasePlugin = new Net("enp0s31f6", 5, [{ button: 1, command: "/usr/bin/nm-applet" }]);
+  const wlp4s0: BasePlugin = new Net("wlp4s0", 5, [{ button: 1, command: "/usr/bin/nm-applet" }]);
   const release: BasePlugin = new Release("Release", 3600);
   const hostname: BasePlugin = new Hostname("Hostname", 3600);
   const battery0: BasePlugin = new Battery("Bat0", 10, "Bat0");
   const battery1: BasePlugin = new Battery("Bat1", 10, "Bat1");
   const uptime: BasePlugin = new Uptime("Uptime", 1);
   const clock: BasePlugin = new Clock("Clock", 1);
-  plugins.push(enp0s3, release, hostname, battery0, battery1, uptime, clock);
+  plugins.push(enp0s31f6, wlp4s0, release, hostname, battery0, battery1, uptime, clock);
   logger.info("Start nodebar");
   app.setPlugins(plugins);
   logger.debug(app);
